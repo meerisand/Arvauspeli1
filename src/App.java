f5a763b7ecb7a4d94ed828894de5206702e318c0
Meeri Sandrén <meeri.sandren@gmail.com>
2024-09-20 14:43:36 +0300

    Initial commit

---
+++ src/App.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class App {
+
+    public static void main(String[] args) throws Exception {
+        
+        Scanner in = new Scanner(System.in);
+        String nimi = "";
+        int arvaus = 0;
+
+        do {
+        System.out.println("Arvaa nimeni. (Jos haluat lopettaa, kirjoita loppu)");
+        nimi = in.nextLine();
+        arvaus++;
+        } while (!nimi.equals( "loppu") && !nimi.equals("Meeri"));
+        
+        if (nimi.equals("Meeri")) {
+            System.out.println("Onnittelut!");
+            System.out.println("Arvasit " + arvaus + " kertaa.");
+        }
+        else {
+            System.out.println("Arvasit " + (arvaus -1) + " kertaa.");
+        }
+        
+    }
+}
