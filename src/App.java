c5d35c2ee9ae01036d3e057f2b8e818cae32fbc2
Meeri Sandrén <meeri.sandren@gmail.com>
2024-09-20 15:24:03 +0300

    Piste pois

--- src/App.java
+++ src/App.java
@@ -9,7 +9,7 @@ public class App {
         int arvaus = 0;
 
         do {
-        System.out.println("Arvaa nimeni. (Jos haluat lopettaa, kirjoita loppu!)");
+        System.out.println("Arvaa nimeni (Jos haluat lopettaa, kirjoita loppu!)");
         nimi = in.nextLine();
         arvaus++;
         } while (!nimi.equals( "loppu!") && !nimi.equals("Merri"));
