b1594eb838ac2c9dc203f6f8c2c87fdbe2bca1f8
Meeri Sandrén <meeri.sandren@gmail.com>
2024-09-20 14:49:36 +0300

    Update App.java

--- src/App.java
+++ src/App.java
@@ -12,9 +12,9 @@ public class App {
         System.out.println("Arvaa nimeni. (Jos haluat lopettaa, kirjoita loppu!)");
         nimi = in.nextLine();
         arvaus++;
-        } while (!nimi.equals( "loppu") && !nimi.equals("Meeri"));
+        } while (!nimi.equals( "loppu!") && !nimi.equals("Meeri"));
         
-        if (nimi.equals("Meeri")) {
+        if (nimi.equals("Meri")) {
             System.out.println("Onnittelut!");
             System.out.println("Arvasit " + arvaus + " kertaa.");
         }
